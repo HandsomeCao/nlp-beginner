c2a1b06fa0a411a91792031c48e99cb75e53fd6c
Handsomecao <cs18281902202@gmail.com>
2019-07-11 23:18:11 +0800

    add util

---
+++ task1/model.py
@@ -0,0 +1,5 @@
+#-*- coding:utf-8 -*-
+
+
+class BowModel(object):
+    def __init__(self, )
---
+++ task1/util.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import re
+import numpy as np
+from tqdm import tqdm
+from collections import Counter
+
+_FILE_PATH = './data/train.tsv'
+_PUNCTUATION = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+_WORD_REGEX = re.compile(r"(?u)\b\w\w+\b")
+_PUNC_TABLE = str.maketrans("", "", _PUNCTUATION)
+_STOP_WORDS = []
+with open('./stopwords.txt', 'r') as f:
+    _STOP_WORDS = f.readlines()
+
+
+def tokenize_words(line, lowercase=True, filter_stopwords=True):
+    words = _WORD_REGEX.findall(line.lower() if lowercase else line)
+    return remove_stop_words(words) if filter_stopwords else words
+
+
+def remove_stop_words(words):
+    return [word for word in words if word not in _STOP_WORDS]
+
+
+class Vocab(object):
+    def __init__(self, fp=_FILE_PATH, mode='train', lowercase=True, filter_punc=True):
+        self.datasets = self._read_tsv(fp, mode)
+        self.word2idx, self.idx2word = self._word_dict(self.datasets)
+        # self.mat = self._mat()
+        self.labels = np.array([data[1] for data in self.datasets]) \
+            if mode == 'train' else np.zeros((1, len(self.datasets)))
+
+    def __len__(self):
+        return len(self.word2idx)
+
+    def _mat(self):
+        lines = [tokenize_words(data[0]) for data in self.datasets]
+        voc_mat = [self.line2BowVec(line) for line in tqdm(lines)]
+        return np.array(voc_mat)
+
+    def line2BowVec(self, line):
+        vec = len(self.word2idx) * [0]
+        for k, v in Counter(line).items():
+            vec[self.word2idx[k]] = v
+        return vec
+
+    def _word_dict(self, datasets):
+        words = []
+        for dataset in datasets:
+            words += tokenize_words(dataset[0])
+        words = list(set(words))
+        word2idx = {word: i for i, word in enumerate(words)}
+        idx2words = {i: word for i, word in enumerate(words)}
+        return word2idx, idx2words
+
+    def _read_tsv(self, fp, mode='train'):
+        datasets = []
+        with open(fp, 'r', encoding='utf-8') as fr:
+            for lidx, line in enumerate(fr):
+                if lidx == 0:
+                    continue
+                lines = line.strip().split('\t')
+                # print(len(lines))
+                if len(lines) == 4 and mode == 'train':
+                    phase, label = lines[2], lines[3]
+                    datasets.append([phase, int(label)])
+                if len(lines) == 3 and mode == 'test':
+                    datasets.append(lines[-1])
+        print(len(datasets))
+        return datasets
+
+
+if __name__ == "__main__":
+    vocab = Vocab()
+    print(len(vocab))
+    print(vocab.labels)
